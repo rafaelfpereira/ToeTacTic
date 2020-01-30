a558ad06453b306a7ac41b4aaa4b38b5e4c05316
Rafael Pereira <rafaelpereira@MB-002250.local>
2020-01-30 10:06:29 -0300

    eslint config

--- ToeTacTicApp/.eslintrc.js
+++ ToeTacTicApp/.eslintrc.js
@@ -1,4 +1,42 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+{
+  "extends": ["airbnb"],
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "jest": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "no-underscore-dangle": ["off"],
+    "import/prefer-default-export": ["off"],
+    "import/no-unresolved": ["error", { "ignore": ["^@"] }],
+    "jsx-a11y/href-no-hash": ["off"],
+    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
+    "semi": ["error", "never"],
+    "react/jsx-props-no-spreading": ["off"],
+    "max-len": [
+      "error",
+      {
+        "code": 80,
+        "tabWidth": 2,
+        "comments": 80,
+        "ignoreComments": false,
+        "ignoreTrailingComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+        "ignoreRegExpLiterals": true
+      }
+    ]
+  },
+  "settings": {
+    "import/parsers": {
+      "babel-eslint": [".js", ".jsx"]
+    }
+  }
+}
